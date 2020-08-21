24da3bd9cbb345bdaf7567161b8e762421f0138e
julian1026 <33234916+julian1026@users.noreply.github.com>
2020-08-21 10:49:50 -0500

    Update ejercicio10.py

--- ejercicio10.py
+++ ejercicio10.py
@@ -4,7 +4,7 @@ suspendidos=0
 
 for alumnos in range(1,16):
     nota= int(input(f"ingrese nota del alumno {alumnos} : "))
-    if nota >3:
+    if nota >=3:
         aprobados+=1
     else:
         suspendidos+=1
